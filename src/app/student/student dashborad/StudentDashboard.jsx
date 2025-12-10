cbf7dd1e2a0e3515b9f586100a558454f5457708
kirithikasrini <kirithikasrini04@gmail.com>
2025-12-10 13:34:24 +0530

    update student dashboard outpass flow

--- src/app/student/student dashborad/StudentDashboard.jsx
+++ src/app/student/student dashborad/StudentDashboard.jsx
@@ -3,21 +3,23 @@ import StudentInfo from './StudentInfo'
 import ParentInfo from './ParentInfo'
 import HostelInfo from './HostelInfo'
 import LeaveInfo from './LeaveInfo'
+import OutpassStatus from './OutpassStatus'
 
 const StudentDashboard = () => {
   return (
     <div>
       <StudentInfo />
-     <section className='flex gap-6 mb-6'>
-       <div className='flex-8'>
-        <ParentInfo />
-       </div>
-       <div className='flex-2'>
-        <HostelInfo />
-       </div>
-     </section>
-     <LeaveInfo />
-    </div>
+      <section className='flex gap-6 mb-6'>
+        <div className='flex-8'>
+          <ParentInfo />
+        </div>
+        <div className='flex-2'>
+          <HostelInfo />
+        </div>
+      </section>
+      <LeaveInfo />
+      <OutpassStatus />
+    </div >
   )
 }
 
